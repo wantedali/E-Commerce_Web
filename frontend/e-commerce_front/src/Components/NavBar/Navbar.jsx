043097a9495827575ee3beb2d6e3c5ce4353094c
Omaratef215 <104634722+Omaratef215@users.noreply.github.com>
2024-04-26 14:25:48 +0300

    navbar-commit-one

--- frontend/e-commerce_front/src/Components/NavBar/Navbar.jsx
+++ frontend/e-commerce_front/src/Components/NavBar/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
+import logo from './Assets/logo.png'
+import cart_icon from './Assets/cart_icon.png'
 
 const Navbar = () => {
 
+    const [menu , setmenu] = useState("shop");
+
     return (
         <div className="navbar">
-
+          <div className="nav-logo">
+        <img src={logo} alt=""/>
+        <p>E-Shop</p>
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setmenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("Men")}}>Men{menu==="Men"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("Women")}}>Women{menu==="Women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setmenu("Kids")}}>Kids{menu==="Kids"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt="" />
+            <div className="nav-cart-count">0</div>
+            </div>
         </div>
     )
 };
