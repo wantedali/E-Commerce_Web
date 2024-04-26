062febfd32f2a46f39bc93d9ef01e88c7ec04939
OmarSaid35 <131901268+OmarSaid35@users.noreply.github.com>
2024-04-27 00:45:03 +0300

    hero_update

--- frontend/e-commerce_front/src/Components/Hero/Hero.jsx
+++ frontend/e-commerce_front/src/Components/Hero/Hero.jsx
@@ -1,33 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import hand_icon from '../Assets/hand_icon.png'; // Assuming you have imported your images correctly
+import hand_icon from '../Assets/hand_icon.png';
 import arrow_icon from '../Assets/arrow.png';
-import hero_image1 from '../Assets/hero_image.png'; // New image imports
-import hero_image2 from '../Assets/hero_image2.png'; // New image imports
-import './Hero.css'; // Import your CSS file
+import hero_image1 from '../Assets/hero_image.png';
+import hero_image2 from '../Assets/hero_image6.jpg';
+import './Hero.css';
 
 const Hero = () => {
-    const [currentImage, setCurrentImage] = useState(hero_image1); // Initial image
+    const [currentImage, setCurrentImage] = useState(hero_image1);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     useEffect(() => {
-        // Function to switch between images
-        
+
         const switchImages = () => {
-            setCurrentImage(currentImage === hero_image1 ? hero_image2 : hero_image1);
+            setIsAnimating(true);
+            setTimeout(() => {
+                setCurrentImage(currentImage === hero_image1 ? hero_image2 : hero_image1);
+                setIsAnimating(false);
+            }, 300);
         };
 
-        // Set interval to switch images every 5 seconds (adjust as needed)
-        const intervalId = setInterval(switchImages, 3000);
 
-        // Cleanup function to clear interval on component unmount
+        const intervalId = setInterval(switchImages, 3500);
+
+
         return () => clearInterval(intervalId);
-    }, [currentImage]); // Depend on currentImage to re-run effect when it changes
+    }, [currentImage]);
 
     return (
         <div className="hero">
             <div className='hero-left'>
-                <h2>
-                    NEW ARRIVALS ONLY
-                </h2>
+                <h2>NEW ARRIVALS ONLY</h2>
                 <div>
                     <div className='hero-hand-icon'>
                         <p>New</p>
@@ -37,13 +39,11 @@ const Hero = () => {
                     <p>For Everyone</p>
                 </div>
                 <div className="hero-latest-btn">
-                    <div>
-                        Latest Collection
-                    </div>
+                    <div>Latest Collection</div>
                     <img src={arrow_icon} alt="" />
                 </div>
             </div>
-            <div className='hero-right'>
+            <div className={`hero-right ${isAnimating ? 'animate' : ''}`}>
                 <img src={currentImage} alt="" />
             </div>
         </div>
