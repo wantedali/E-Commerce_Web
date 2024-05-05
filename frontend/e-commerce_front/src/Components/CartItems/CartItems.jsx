5759f336d2b85225f29498d18479490f258c1923
OmarSaid35 <131901268+OmarSaid35@users.noreply.github.com>
2024-05-05 03:04:30 +0300

    Final Cart_items page

--- frontend/e-commerce_front/src/Components/CartItems/CartItems.jsx
+++ frontend/e-commerce_front/src/Components/CartItems/CartItems.jsx
@@ -3,72 +3,128 @@ import './CartItems.css'
 import chevronleftnormal2x from '../Assets/chevronleftnormal@2x.png'
 import vector_cart from '../Assets/vector-cart.svg'
 import vector1_cart from '../Assets/vector1-cart.svg'
+import plus from '../Assets/plus.png'
+import minus from '../Assets/minus.png'
 import { ShopContext } from '../../Contexts/ShopContext'
 import { useContext } from "react";
+import question from '../Assets/question-mark@2x.png'
+import boxes from '../Assets/boxes-and-shopping-bag-with-discount-symbol@2x.png';
 
 const CartItems = () => {
-    const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+    const { removeFromCart2, clearCart, getTotalCartItems, getTotalCartAmount, all_product, cartItems, removeFromCart, addToCart } = useContext(ShopContext);
+    const cartHasItems = Object.values(cartItems).some(quantity => quantity > 0);
+
+    const handleClearCart = () => {
+        clearCart();
+    }
     return (
         <div class="new-cart-items">
-            <div class="new-cart">Cart</div>
-            <div class="new-items">2 items</div>
-            <button class="new-empty-cart">Empty Cart</button>
-            <div class="new-cart-items-child"></div>
+            <div class="new-cart2">
 
-            {all_product.map((e) => {
 
-                if (cartItems[e.id] > 0) {
-                    return (
-                        <div class="new-rectangle-parent">
-                            <div class="new-group-child"></div>
-                            <img class="new-group-item" alt="" src={e.image} />
-                            <div class="new-rectangle-group">
-                                <div class="new-group-inner"></div>
-                                <div class="new-women">{e.category}</div>
-                            </div>
-                            <button class="new-mdiheart-outline">
-                                <img class="new-vector-icon" alt="" src={vector_cart} />
+                <div class="new-cart">Cart</div>
+                {cartHasItems && (
+                    <>
+                        <div class="new-items"><span>{getTotalCartItems()}</span> items</div>
+                        <button class="new-empty-cart" onClick={handleClearCart}>Empty Cart</button>
+                        <div class="new-cart-items-child"></div>
+                    </>
+                )}
+
+
+                {all_product.map((e) => {
+
+                    if (cartItems[e.id] > 0) {
+                        return (
+                            <div class="new-rectangle-parent">
+                                <div class="new-group-child"></div>
+                                <img class="new-group-item" alt="" src={e.image} />
+                                <div class="new-rectangle-group">
+                                    <div class="new-group-inner">
+                                        <div class="new-women">{e.category}</div>
+                                    </div>
+
+                                </div>
+                                <button class="new-mdiheart-outline">
+                                    <img class="new-vector-icon" alt="" src={vector_cart} />
+                                </button>
+                                <button class="new-tablertrash" onClick={() => { removeFromCart(e.id) }}>
+                                    <img class="new-vector-icon1" alt="" src={vector1_cart} />
+                                </button>
+                                <div class="new-striped-flutter-sleeve">
+                                    {e.name}
+                                </div>
+                                <div class="new-rectangle-div">
+                                    <button class="new-plus" onClick={() => addToCart(e.id)}>
+                                        <div>
+                                            <img class="new-plus-icon" alt="" src={plus} />
+                                        </div>
+                                    </button>
+                                    <button class="new-minus" onClick={() => removeFromCart2(e.id)}>
+                                        <div>
+                                            <img class="new-cart-items-inner" src={minus} alt="" />
+                                        </div>
+                                    </button>
+                                </div>
+                                <div class="new-div">{cartItems[e.id]}</div>
+                                <div class="new-egp230">${e.new_price}</div>
+                                <div class="new-egp230">${e.new_price}</div>
+                            </div>);
+                    }
+                    return null;
+
+                })}
+
+                {cartHasItems && (
+                    <>
+                        <div class="new-subtotal-parent">
+                            <div class="new-subtotal">Subtotal</div>
+                            <div class="new-free">EGP{getTotalCartAmount()}</div>
+                        </div>
+                        <img src="" alt="" />
+                        <div class="new-total-parent">
+                            <b class="new-total">Total</b>
+                            <b class="new-egp6211">EGP{getTotalCartAmount()}</b>
+                        </div>
+                        <div class="new-shipping-parent">
+                            <div class="new-subtotal">Shipping</div>
+                            <div class="new-free">Free</div>
+                        </div>
+                        <div class="new-cart-items-item"></div>
+                        <div class="new-i-have-a">I have a promocode</div>
+                        <img class="new-chevron-left-normal-icon" alt="" src={chevronleftnormal2x} />
+                        <button class="new-rectangle-container">
+                            <div class="new-group-child1"></div>
+                            <div class="new-checkout">Checkout</div>
+                        </button>
+                    </>
+                )}
+
+                {!cartHasItems && (
+                    <>
+                        <div class="cart-items2">
+                            <div class="nothing-in-here">Nothing in Here!</div>
+                            <div class="your-cart-is">Your cart is currently empty.</div>
+                            <button class="rectangle-parent-no">
+                                <div class="group-child-no"></div>
+                                <div class="continue-shopping">Continue Shopping</div>
                             </button>
-                            <button class="new-tablertrash">
-                                <img class="new-vector-icon1" alt="" src={vector1_cart} onClick={() => { removeFromCart(e) }} />
-                            </button>
-                            <div class="new-striped-flutter-sleeve">
-                                {e.name}
-                            </div>
-                            <button class="new-group-button">
-                                <div class="new-group-child2"></div>
-                                <div class="new-group-child3"></div>
-                            </button>
-                            <div class="new-rectangle-div"></div>
-                            <div class="new-div">{cartItems[e.id]}</div>
-                            <div class="new-egp230">${e.new_price}</div>
-                            <div class="new-egp230">${e.new_price}</div>
-                        </div>);
-                }
-                return null;
+                            <img
+                                class="boxes-and-shopping-bag-with-di"
+                                alt=""
+                                src={boxes}
+                            />
 
-            })}
+                            <img
+                                class="question-mark-icon"
+                                alt=""
+                                src={question}
+                            />
+                        </div>
+                    </>
+                )}
 
-            <div class="new-subtotal-parent">
-                <div class="new-subtotal">Subtotal</div>
-                <div class="new-free">EGP621</div>
             </div>
-            <div class="new-total-parent">
-                <b class="new-total">Total</b>
-                <b class="new-egp6211">EGP621</b>
-            </div>
-            <div class="new-shipping-parent">
-                <div class="new-subtotal">Shipping</div>
-                <div class="new-free">Free</div>
-            </div>
-            <div class="new-cart-items-item"></div>
-            <div class="new-i-have-a">I have a promocode</div>
-            <img class="new-chevron-left-normal-icon" alt="" src={chevronleftnormal2x} />
-            <button class="new-rectangle-container">
-                <div class="new-group-child1"></div>
-                <div class="new-checkout">Checkout</div>
-            </button>
-            <button class="new-cart-items-inner"></button>
         </div>
 
 
--- frontend/e-commerce_front/src/Contexts/ShopContext.jsx
+++ frontend/e-commerce_front/src/Contexts/ShopContext.jsx
@@ -1,15 +1,15 @@
-import React, { createContext,useState } from "react";
+import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
 export const ShopContext = createContext(null);
 
-const getDefaultCart = ()=>{
+const getDefaultCart = () => {
 
     let cart = {};
 
     for (let index = 0; index < all_product.length; index++) {
         cart[index] = 0;
-        
+
     }
     return cart;
 }
@@ -20,16 +20,50 @@ const ShopContextProvider = (props) => {
 
     const addToCart = (itemId) => {
 
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         console.log(cartItems)
     }
 
     const removeFromCart = (itemId) => {
 
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    const removeFromCart2 = (itemId) => {
+        setCartItems((prev) => {
+            const updatedCartItems = { ...prev };
+            updatedCartItems[itemId] = Math.max(updatedCartItems[itemId] - 1, 0); // Ensure the quantity doesn't go below 0
+            return updatedCartItems;
+        });
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItems = () => {
+        let totalItems = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItems += cartItems[item];
+            }
+        }
+        return totalItems;
+    }
+
+    const clearCart = () => {
+        setCartItems(getDefaultCart());
+    }
+
+    const contextValue = { removeFromCart2, clearCart,getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
