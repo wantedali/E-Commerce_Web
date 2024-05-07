be8decdfc5560a0b72c227338d162ee48eed9811
wantedali <134491027+wantedali@users.noreply.github.com>
2024-05-07 21:11:27 +0300

    side

--- admin/src/Compenents/Navbar/Navbar.jsx
+++ admin/src/Compenents/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import navlogo from '../../assets/nav-logo.svg';
 import navProfile from '../../assets/nav-profile.svg';
 const Navbar=()=>{
     return(
-        <div className='nav'>
+        <div className='navbar'>
             <img src={navlogo} alt="" className="nav-logo" />
             <img src={navProfile}  className='nav-profile'alt=""/>
         </div>
