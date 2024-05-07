6ade6ca3acf1af37987e6703dde1c81dc686f1dd
wantedali <134491027+wantedali@users.noreply.github.com>
2024-05-07 19:22:11 +0300

    nav

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Navbar from './Compenents/Navbar/Navbar';
+import Navbar from "./Compenents/Navbar/Navbar";
+
 const App=()=>{
   return (
     <div>
-      <Navbar />
+      <Navbar/>
     </div>
   )
 }
